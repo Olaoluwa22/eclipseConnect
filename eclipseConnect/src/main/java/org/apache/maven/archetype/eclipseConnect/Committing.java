e49600ce42bf6372c459802fae356fd8a636f778
HP <HP@Rashford>
2023-05-17 14:30:12 +0100

    first commit

---
+++ eclipseConnect/src/main/java/org/apache/maven/archetype/eclipseConnect/Committing.java
@@ -0,0 +1,12 @@
+package org.apache.maven.archetype.eclipseConnect;
+
+public class Committing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I think I found a solution...");
+
+	}
+
+}
