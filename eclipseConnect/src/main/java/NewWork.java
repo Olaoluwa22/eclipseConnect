661114aa2337e535c2c1f018afc171899a8fd4f8
HP <HP@Rashford>
2023-05-17 12:04:36 +0100

    first commit

---
+++ eclipseConnect/src/main/java/NewWork.java
@@ -0,0 +1,9 @@
+
+public class NewWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("All I am doing is, trying to make my first commit and push it...");
+	}
+
+}
